2bb85dccb5660f6b77b2f99e1f6c0526e3a7f8f3
tomtommysid <tomtommysid@gmail.com>
2023-04-12 12:37:21 +0900

    notice, customer_service 프론트 변경, notice list, write

--- src/main/java/com/itwillbs/yata/HomeController.java
+++ src/main/java/com/itwillbs/yata/HomeController.java
@@ -1,41 +1,32 @@
 package com.itwillbs.yata;
 
-import java.util.*;
-
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.itwillbs.yata.service.*;
-import com.itwillbs.yata.vo.*;
 
 @Controller
 public class HomeController {
-
-	@Autowired
-	private ReviewService service;
-
-	@GetMapping("/")
-	public String home(Model model) {
-		List<ReviewVO> reviewList = service.getReview();
-	    System.out.println(reviewList);
-	    model.addAttribute("review", reviewList);
-	    return "index";
-	}
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		return "index";
+	}
 	@GetMapping("event")
 	public String event() {
 		return "notice/event";
 	}
-
 	@GetMapping("notice_view")
 	public String notice_view() {
-		return "notice/notice_view";
+//		return "notice/notice_list";
+		return "notice/notice";
 	}
-
 	@GetMapping("customer_service")
 	public String notice_service() {
-		return "customer/customer_service";
+		return "customer/customer_service_ke";
 	}
+	
+	
 }
